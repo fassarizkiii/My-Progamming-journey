c8b16b7d5154952fe66d4bd2b8a8db5e983061d4
fassarizkiii <fassarizkiramadhan@gmail.com>
2024-11-06 20:34:03 -0800

    LKPD_10
    Praktek LKPD 10 menghasilkan tulisan "TIDAK" dan kalau jika di tekan tombol K1 akan muncul tulisan "YA"

---
+++ LKPD10_FASSA_RR.ino
@@ -0,0 +1,16 @@
+//fassa rizki r
+//XI TAV 1
+const int buttonPin = 26;
+void setup(){
+  pinMode(buttonPin,INPUT);
+  Serial.begin(9600);}
+void loop(){
+  int buttonState = digitalRead(buttonPin);
+  Serial.print("Tombol ditekan: ");
+  if (buttonState == HIGH) {
+    Serial.println("YA");
+  } else {
+    Serial.println("TIDAK");}
+  delay(100);}
+  
+  
